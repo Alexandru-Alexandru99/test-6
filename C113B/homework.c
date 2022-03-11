9bc7ad09ede47f41619f71011e2fdadccb8dbb37
alexandru.alexandru <alexandru.alexandru@mta.ro>
2022-03-11 10:56:09 +0200

    Fri, Mar 11, 2022 10:56:09 AM

---
+++ C113B/homework.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("hello world!\n");
+
+    for (int i = 0; i < 100; i++)
+    {
+        printf("Hello world!\n");
+        printf("Hello world!\n");
+        printf("Hello world!\n");
+        printf("Hello world!\n");
+        printf("Hello world!\n");
+        printf("Hello world!\n");
+        printf("hello world!\n");
+    }
+    return 0;
+}
