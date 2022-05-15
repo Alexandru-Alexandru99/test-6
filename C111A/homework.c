a7c82ed71c2bec5f843efea226f5284bd1262f34
Alexandru-Alexandru99 <lemonation99@gmail.com>
2022-05-15 19:43:40 +0300

    Sun May 15 19:43:40 EEST 2022

--- C111A/homework.c
+++ C111A/homework.c
@@ -20,5 +20,27 @@ int main()
         printf("Hello world!\n");
         printf("hello world!\n");
     }
+
+    for (int i = 0; i < 100; i++)
+    {
+        printf("Hello world!\n");
+        printf("Hello world!\n");
+        printf("Hello world!\n");
+        printf("Hello world!\n");
+        printf("Hello world!\n");
+        printf("Hello world!\n");
+        printf("hello world!\n");
+    }
+    for (int i = 0; i < 100; i++)
+    {
+        printf("Hello world!\n");
+        printf("Hello world!\n");
+        printf("Hello world!\n");
+        printf("Hello world!\n");
+        printf("Hello world!\n");
+        printf("Hello world!\n");
+        printf("hello world!\n");
+    }
+
     return 0;
 }
